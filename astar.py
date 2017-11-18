fe2a00b96d758bd7fae356f00f68dd4ac79fd853
BuildTools <polemonium@users.noreply.github.com>
2017-11-18 12:31:06 +0100

    Added path, fixed search, removed time.sleep

--- astar.py
+++ astar.py
@@ -13,34 +13,39 @@ def astar(sp, gp, tp1, tp2, order):
     ###
     x = sp[0]
     y = sp[1]
-    nextmove = []
-    # stack = [[[x, y]]]
-    # var_c = []
     heap = []
-    heappush(heap, (gp[0]-x+gp[1]-y,0, x, y))
-    #heappush(heap, (euklid, x, y))
+    heappush(heap, (gp[0]-x+gp[1]-y,0, x, y, [[x,y]]))
     while True:
         if main.goalnotreached(x, y):
             main.setmarker(x, y)
+            topush = []
             if(main.neighbourfree('u', x, y)):
                 euklid = gp[0] - (x-1) + (gp[1] - y)
-                heappush(heap, (heap[0][1]+euklid,heap[0][1]+1, x-1, y))
+                u = (heap[0][1]+euklid,heap[0][1]+1, x-1, y, heap[0][4]+[[x-1, y]])
+                topush.append(u)
             if(main.neighbourfree('r', x, y)):
                 euklid = gp[0] - x + (gp[1] - (y+1))
-                heappush(heap, (heap[0][1]+euklid,heap[0][1]+1, x, y+1))
+                r = (heap[0][1]+euklid,heap[0][1]+1, x, y+1, heap[0][4]+[[x, y+1]])
+                topush.append(r)
             if(main.neighbourfree('d', x, y)):
                 euklid = gp[0] - (x+1) + (gp[1] - y)
-                heappush(heap, (heap[0][1]+euklid,heap[0][1]+1, x+1, y))
+                d = (heap[0][1]+euklid,heap[0][1]+1, x+1, y, heap[0][4]+[[x+1, y]])
+                topush.append(d)
             if(main.neighbourfree('l', x, y)):
                 euklid = gp[0] - x + (gp[1] - (y-1))
-                heappush(heap, (heap[0][1]+euklid,heap[0][1]+1, x, y-1))
+                l = (heap[0][1]+euklid,heap[0][1]+1, x, y-1, heap[0][4]+[[x, y-1]])
+                topush.append(l)
+            heappop(heap)
+            while not len(topush) == 0:
+                heappush(heap, topush[0])
+                topush.pop(0)
+            #print(heap)
             x = heap[0][2]
             y = heap[0][3]
-            heappop(heap)
-            print(heap)
             output.printstate()
-            time.sleep(.10)
+            #time.sleep(.10)
         else:
+            print(heap[0][4])
             break
 
         # else:
