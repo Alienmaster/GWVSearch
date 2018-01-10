cd8a74cf2e60fd35777a635b6232ec525430fc3a
BuildTools <polemonium@users.noreply.github.com>
2018-01-10 22:55:40 +0100

    Added Project folder and movement.

---
+++ Project/gwv.py
@@ -0,0 +1,64 @@
+Input = [[],[],[],[]]
+#Goal = [[1,2,3,4][5,6,7,8][9,10,11,12][13,14,15,X]]
+Moves = []
+
+
+
+def movePossible():
+	return True
+
+
+def moveLeft():
+	global Input
+	if movePossible():
+		for i in range(0, len(Input)-1):
+			for j in range(0,len(Input[i])-1):
+				if Input[i][j] == 'X':
+					t = Input[i][j-1]
+					Input[i][j-1] = 'X'
+					Input[i][j] = t
+					return
+
+def moveRight():
+	global Input
+	if movePossible():
+		for i in range(0, len(Input)-1):
+			for j in range(0,len(Input[i])-1):
+				if Input[i][j] == 'X':
+					t = Input[i][j+1]
+					Input[i][j+1] = 'X'
+					Input[i][j] = t
+					return
+
+def moveUp():
+	global Input
+	if movePossible():
+		for i in range(0, len(Input)-1):
+			for j in range(0,len(Input[i])-1):
+				if Input[i][j] == 'X':
+					t = Input[i-1][j]
+					Input[i-1][j] = 'X'
+					Input[i][j] = t
+					return
+
+def moveDown():
+	global Input
+	if movePossible():
+		for i in range(0, len(Input)-1):
+			for j in range(0,len(Input[i])-1):
+				if Input[i][j] == 'X':
+					t = Input[i+1][j]
+					Input[i+1][j] = 'X'
+					Input[i][j] = t
+					return
+
+Input = [	[1,'X',3,4],
+			[5,2,7,8],
+			[9,6,11,12],
+			[13,10,14,15]]
+
+moveDown()
+print(Input)
+moveUp()
+print(Input)
+
