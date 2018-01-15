b418d1c5c08e28580cf504a67b0260657927353f
Marc Strothmann <3strothm@informatik.uni-hamburg.de>
2018-01-15 20:48:24 +0100

    Heuristiken vorerst in einer separaten Datei Heuristik: Manhatten Distanz Heuristik: Manhatten Distanz + Lineare Konflikte TODO: Weitere Heuristiken

---
+++ Project/heuristics.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import math
+
+def getGoalCoordinates(matrix):
+    goalCoords = []    
+    for v in range(0, len(matrix)*len(matrix[0])):
+        if v != 0:
+            xGoal = (v - 1) % len(matrix)
+            yGoal = math.floor((v - 1)/len(matrix))
+            goalCoords.append((xGoal,yGoal))
+    return goalCoords
+        
+
+"""Calutates the Manhatten Distance for the given puzzle
+   as the sum of all distances from current position to goal position
+   for each tile in the puzzle"""
+def calcMD(matrix):
+    distance = 0
+    goalCoords = getGoalCoordinates(matrix)
+    for y in range(0, len(matrix)):
+        for x in range(0, len(matrix[y])):
+            if matrix[y][x] != 0:
+                xGoal, yGoal = goalCoords[matrix[y][x] - 1]
+                distance += abs(x - xGoal) + abs(y - yGoal)            
+    
+    return distance
+
+"""Calculates the Linear Conflict
+   A linear conflict occurs when two tiles are in the same row or collumn
+   as their goal position, but one or both tiles are blocked from reaching
+   their goal by the other tile. In those cases, the distance will be increased
+   by 2 for the two steps necessary for one tile to move out of the way."""
+def calcLC(matrix):
+    distance = calcMD(matrix)
+    goalCoords = getGoalCoordinates(matrix)
+    for y in range(0, len(matrix)):
+        for x in range(0, len(matrix)):
+            if matrix[y][x] != 0:
+                xGoal, yGoal = goalCoords[matrix[y][x]-1]
+                """check for conflicts in row"""
+                if y == yGoal:
+                    for i in range(1 + x, len(matrix[y])):
+                        if (y == goalCoords[matrix[y][i]-1][1] and 
+                            goalCoords[matrix[y][x]-1][0] > goalCoords[matrix[y][i]-1][0]):
+                            distance += 2
+              
+                """check for conflicts in collumn"""
+                if x == xGoal:
+                    for i in range(1 + y, len(matrix)):
+                        if (x == goalCoords[matrix[i][x]-1][0] and
+                            goalCoords[matrix[y][x]-1][1] > goalCoords[matrix[i][x]-1][1]):
+                            distance += 2
+    return distance
+
+#TODO: Alternative Heuristics
+
+def calcHeuristic(method, matrix):
+    """
+        Inputs
+            method - chosen heuristic (MD = Manhatten Distance, LC = Linear Conflict)
+            matrix - input matrix of puzzle
+            
+        Output
+            heuristic value
+    """    
+    if method not in ('MD','LC'):
+        sys.exit(1)
+    
+    if method == 'MD':
+        return calcMD(matrix)
+    if method == 'LC':
+        return calcLC(matrix)
+
+
+
+"""Test data"""
+eightMatrix = [(1,2,6),
+               (4,5,3),
+               (8,7,0)]
+fifteenMatrix = [(2,3,1,8),
+                 (5,7,6,4),
+                 (12,11,10,9),
+                 (15,14,13,0)]
+
+print("MD1",calcHeuristic('MD', eightMatrix))
+print("LC1",calcHeuristic('LC', eightMatrix))
+print("MD2",calcHeuristic('MD', fifteenMatrix))
+print("LC2",calcHeuristic('LC', fifteenMatrix))
